00f66686313bf1937f0b6c2b948a26daac5dede8
ne_Sachirou <utakata.c4se@gmail.com>
2017-07-01 15:39:25 +0900

    Setup lint & test

--- lib/pqueue2.ex
+++ lib/pqueue2.ex
@@ -1,18 +1,4 @@
 defmodule Pqueue2 do
   @moduledoc """
-  Documentation for Pqueue2.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pqueue2.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/pqueue2_test.exs
+++ test/pqueue2_test.exs
@@ -1,8 +1,4 @@
 defmodule Pqueue2Test do
   use ExUnit.Case
   doctest Pqueue2
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
